45dd7e4387951ddc6fd749c27b3e493ce363a85e
Kumaravel <kumaravel@student.tce.edu>
2024-08-13 09:21:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Cart from './components/cart'
 import Shop from './components/shop';
 import Display from './components/display';
 import Search from './components/search';
 import Bevarages from './components/bevarages'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+
 function App() {
   
   return (
+    <>
+    <>Hi</>
+    <GoogleOAuthProvider clientId="642146652821-b9ip7s68ea19oq7ptgqqlq4l2fttq82s.apps.googleusercontent.com">
+    <GoogleLogin
+  onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/>;
+
     <div className="App">
+      
         <Search />
         <Shop/>
         <Display />
         <Bevarages />
     </div>
+    </GoogleOAuthProvider>
+    </>
   );
 }
 
--- src/components/shop.js
+++ src/components/shop.js
@@ -4,8 +4,8 @@ function Shop()
 {
     return (
     <div className="shop-heading">
-        <h1 className="text-shop">Compras </h1>
-        <h1 className="text-shop1">Felices</h1>
+        <h1 className="text-shop">Happy </h1>
+        <h1 className="text-shop1">Shopping</h1>
         <img className="fruitsveg-img" src="https://www.eatthis.com/wp-content/uploads/sites/4/2023/07/fruits-and-vegetables.jpg?quality=82&strip=all&w=1200"></img>
     </div>
     );
