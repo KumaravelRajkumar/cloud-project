8241f1b502e53296084298be26c2c43654f86b7a
Kumaravel <kumaravel@student.tce.edu>
2024-10-07 19:09:21 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const expenseModel = require("./models/Expense")
+const expenseModel1 = require("./models/Cart")
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/grocery");
+
+app.post('/additem' , (req,res) => {
+    console.log(req.body);
+    const {name,price,quantity} = req.body;
+    expenseModel.create({name: name, price: price, quantity: quantity})
+    .then(result => res.json("Items stored"))
+    .catch(err => res.json(err))
+})
+
+app.post('/storecart' , (req,res) => {
+    console.log(req.body);
+    const {name,price} = req.body;
+    expenseModel1.create({name: name, price: price})
+    .then(result => res.json("Items stored"))
+    .catch(err => res.json(err))
+})
+
+app.get('/showexpense',(req,res)=>{
+    expenseModel.find()
+    .then(results=> res.json(results))
+    .catch(err=>res.json(err))
+})
+
+
+app.listen(3001,() => {
+    console.log("Server is running")
+})
